3b944b6b65247dbce4923b4dc7f3d072ce8a8a96
Mr13Josh <jswan@unca.edu>
2015-04-29 09:14:36 -0400

    j

--- youtube.js
+++ youtube.js
@@ -43,7 +43,7 @@
     player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: $('#videoValue').value(),
+    videoId: $('#videoValue'),
     playerVars: {
      'controls': 0,
      'disablekb': 1,
